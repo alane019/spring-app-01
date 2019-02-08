f282894e18b92f4b9ebea2a85371182e4596954a
Adam L. Lane <alane019@gmail.com>
2019-02-07 19:49:38 -0500

    first commit

---
+++ src/main/java/com/tts/Restaurant/RestaurantController.java
@@ -0,0 +1,32 @@
+package com.tts.Restaurant;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@Controller
+public class RestaurantController {
+	
+	@Autowired
+	RestaurantRepository restaurantRepository;
+
+    @GetMapping
+    public String index(Restaurant restaurant) {
+	return "restaurant/index";
+    }
+    
+    @PostMapping(value = "/")
+    public String addNewRestaurant(Restaurant restaurant, Model model) {
+    	restaurantRepository.save(restaurant);
+    	model.addAttribute("owner", restaurant.getOwner());
+    	model.addAttribute("name", restaurant.getName());
+    	model.addAttribute("phoneNumber", restaurant.getPhoneNumber());
+    	model.addAttribute("address", restaurant.getAddress());
+    	return "restaurant/result";
+    }
+}
+
+
